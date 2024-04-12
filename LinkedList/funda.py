9e4e9e510caff70e618a61306a2be8d50a550c28
csanjay99 <csanjay99@hotmail.com>
2024-04-12 21:07:46 +0530

    added LL class

--- LinkedList/funda.py
+++ LinkedList/funda.py
@@ -5,3 +5,20 @@ class Node:
 
 new_node = Node(10)
 print(new_node)
+
+# CREATE EMPTY LINKED LIST
+# class LinkedList:
+#     def __init__(self) -> None:
+#         self.head = None
+#         self.tail = None
+#         self.length = 0
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+new_linked_list = LinkedList(10)
+print(new_linked_list.length)
