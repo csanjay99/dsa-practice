cd9f420a3d952d3076b9fb7e76a25902a45f9cb7
csanjay99 <csanjay99@hotmail.com>
2024-04-12 23:14:01 +0530

    more code added

--- LinkedList/funda.py
+++ LinkedList/funda.py
@@ -3,22 +3,42 @@ class Node:
         self.value = value,
         self.next = None
 
+    def __repr__(self) -> str:
+        return f"This Node has value of {self.value} and its next node is {self.next}"
+
 new_node = Node(10)
 print(new_node)
 
 # CREATE EMPTY LINKED LIST
-# class LinkedList:
-#     def __init__(self) -> None:
-#         self.head = None
-#         self.tail = None
-#         self.length = 0
-
 class LinkedList:
-    def __init__(self, value) -> None:
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-new_linked_list = LinkedList(10)
-print(new_linked_list.length)
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else :
+            self.tail.next = new_node
+            self.tail = new_node
+        
+        self.length += 1
+    
+    def __repr__(self) -> str:
+        llstr = f""
+
+new_linked_list = LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+
+print("head", new_linked_list.head)
+print("tail", new_linked_list.tail)
+print("head.value",new_linked_list.head.value)
+print("tail.value",new_linked_list.tail.value)
+print("head.next",new_linked_list.head.next)
+print("tail.next",new_linked_list.tail.next)
+print("length",new_linked_list.length)
+
